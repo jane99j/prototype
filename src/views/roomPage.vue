8534c8547ca89336b349668763abbb4f3584580c
rebornth <kasidinthon444@gmail.com>
2022-12-16 15:52:04 +0700

    Edit roomPage

--- src/views/roomPage.vue
+++ src/views/roomPage.vue
@@ -1,40 +1,43 @@
 <template>
-    <ion-card>
-    <h1> เพิ่มห้องพัก</h1>
-    <ion-list>
+  <ion-card>
+    <ion-card-header>
+      <ion-card-title>เพิ่มห้องพัก</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content>
+      <ion-list>
       <ion-item>
-        <ion-label>เลขห้องพัก</ion-label>
-        <ion-input></ion-input>
+        <ion-label>เลขห้องพัก :</ion-label>
+        <ion-input text placeholder="กรอกเลขห้องพัก"></ion-input>
       </ion-item>
   
       <ion-item>
-        <ion-label>ชั้น</ion-label>
-        <ion-input placeholder=""></ion-input>
+        <ion-label>ชั้น :</ion-label>
+        <ion-input text placeholder="กรอกเลขชั้น"></ion-input>
       </ion-item>
   
       <ion-item>
-        <ion-label>ตึก</ion-label>
-        <ion-input value=""></ion-input>
+        <ion-label>ตึก :</ion-label>
+        <ion-input text placeholder="กรอกชื่อตึก" ></ion-input>
       </ion-item>
   
       <ion-item>
-        <ion-label>ราคาห้อง</ion-label>
-        <ion-input value="" :readonly="true"></ion-input>
+        <ion-label>ราคาห้อง :</ion-label>
+        <ion-input number placeholder="กรอกราคาห้องพัก" ></ion-input>
       </ion-item>
-  
-      <ion-item>
-        <ion-label></ion-label>
-        <ion-button>บันทึกห้องพัก</ion-button>
-      </ion-item>
-    </ion-list>
-</ion-card>
+
+      </ion-list>
+    </ion-card-content>
+    <ion-button expand="block">บันทึก :</ion-button>
+  </ion-card>
+
   </template>
   
   <script lang="ts">
-    import { IonInput, IonItem, IonLabel, IonList,IonButton } from '@ionic/vue';
+    import {  IonCardTitle,IonCard, IonCardContent, IonCardHeader,IonInput, IonItem, IonLabel, IonList,IonButton } from '@ionic/vue';
     import { defineComponent } from 'vue';
   
     export default defineComponent({
-      components: { IonInput, IonItem, IonLabel, IonList,IonButton },
+      components: { IonCardTitle,IonCard, IonCardContent, IonCardHeader,IonInput, IonItem, IonLabel, IonList,IonButton },
     });
   </script>
