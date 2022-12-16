a59b458b925cc5faa86b107c5eb10935effa26ed
Rocky0191 <Rocky0191>
2022-12-16 15:26:16 +0700

    หน้าเพิ่มห้องพัก

---
+++ src/views/waterelectricitybillPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>ค่าน้ำ</h1>
+    <ion-list>
+      <ion-item>
+        <ion-select placeholder="หน่วยค่าน้ำ">
+        <ion-select-option value="apples">5</ion-select-option>
+        <ion-select-option value="oranges">10</ion-select-option>
+        <ion-select-option value="bananas">15</ion-select-option>
+      </ion-select>
+      </ion-item>
+  
+      <ion-item>
+        <ion-label>ระบุหน่วยค่าน้ำ</ion-label>
+        <ion-input placeholder="หน่วยค่าน้ำ"></ion-input>
+      </ion-item>
+      <h1>ค่าไฟ</h1>
+      <ion-item>
+        <ion-label>ระบุหน่วยค่าไฟ</ion-label>
+        <ion-input placeholder="หน่วยค่าไฟ"></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-label></ion-label>
+        <ion-button>บันทึก</ion-button>
+      </ion-item>
+    </ion-list>
+  </template>
+  
+  <script lang="ts">
+    import { IonInput, IonItem, IonLabel, IonList } from '@ionic/vue';
+    import { defineComponent } from 'vue';
+  
+    export default defineComponent({
+      components: { IonInput, IonItem, IonLabel, IonList },
+    });
+  </script>
