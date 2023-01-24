efb9c2645e0c985613c6774829c8993d1ee0fd9d
Rocky0191 <Rocky0191>
2023-01-24 17:51:14 +0700

    Co-authored-by: jane99j <jane99j@users.noreply.github.com>

--- src/views/Room/testPage.vue
+++ src/views/Room/testPage.vue
@@ -8,40 +8,41 @@
         <ion-title>{{ $route.params.id }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">{{ $route.params.id }}</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
         <strong class="capitalize">{{ $route.params.id }}</strong>
         <h1>TEST DATABASE</h1>
-        <form action="">
-          <table>
-            <tr>
-              <td>Name</td>
-              <td><input type="text" v-model="friend.name"></td>
-            </tr>
-            <tr>
-                <td>Email</td>
-                <td><input type="Email" v-model="friend.email"></td>
-              </tr>
-              <tr>
-                <td>Phone</td>
-                <td><input type="text" v-model="friend.phone"></td>
-              </tr>
-              <tr>
-                <td></td>
-                <ion-button type="submit" @click="sendData">ยืนยัน</ion-button>
-              </tr>
+        <table class="table- table-bordered ">
+          <tr>
+            <th>fistname</th>
+            <th>lastname</th>
+            <th>user</th>
+          </tr>
+          <tr v-for=" alluser" v-bind:key="alluser.id">
+            <th>{{ alluser.namedormitory }}</th>
+            <th>{{ alluser.addressdormitory }}</th>
+            <th>{{ alluser.phonedormitory }}</th>
+
+          </tr>
 
 
-          </table>
-        </form>
+        </table>
+
+        <div id="app">
+          {{ info }}
+        </div>
+
       </div>
+
+
+
     </ion-content>
   </ion-page>
 </template>
@@ -49,48 +50,29 @@
 <script lang="ts">
 import axios from 'axios';
 import { defineComponent } from 'vue';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar,IonButton } from '@ionic/vue';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, } from '@ionic/vue';
 
 export default defineComponent({
   name: 'FolderPage',
   components: {
-    IonButtons,IonContent,IonHeader,IonMenuButton,IonPage,IonTitle,IonToolbar,IonButton
+    IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar,
   },
-  data(){
-    return{
-      friend:{
-        name:"",
-        email:"",
-        phone:"",
-      }
-    }
-},
-methods:{
-  sendData(){
-    console.log("sendData active");
 
-    axios.post("http://localhost/api.php", {
-      name: this.friend.name,
-      email: this.friend.email,
-      phone: this.friend.phone,
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-    this.clearData();
-  },
-  clearData(){
-    this.friend.name = "";
-    this.friend.phone = "";
-    this.friend.email = "";
-  }
-},
 
 });
 
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost/api_select.php')
+      .then(response => (this.info = response))
+  }
+})
 
 </script>
