d48ca25f1d2bae8a59e6042b6d110eb1b1840236
jane99j <jaynerrch@gmail.com>
2023-02-14 13:57:46 +0700

    update

--- src/views/updateRoom/editroomPage.vue
+++ src/views/updateRoom/editroomPage.vue
@@ -1,158 +1,107 @@
 <template>
   <ion-page>
-      <ion-header :translucent="true">
-    <ion-toolbar color="purple">
-      <ion-buttons slot="start">
-        <ion-menu-button color="primary"></ion-menu-button>
-      </ion-buttons>
-      <ion-title>จัดการห้อง</ion-title>
-    </ion-toolbar>
-  </ion-header>
-    
+    <ion-header :translucent="true">
+      <ion-toolbar color="purple">
+        <ion-buttons slot="start">
+          <ion-menu-button color="primary"></ion-menu-button>
+        </ion-buttons>
+        <ion-title>จัดการห้อง</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">{{ $route.params.id }}</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
         <strong class="capitalize">{{ $route.params.id }}</strong>
         <ion-item>
           <ion-row>
-        <ion-toolbar>
-          <ion-segment  value="all">
-              <ion-segment-button value="all" @click="allroom()">
-                <ion-label>ห้องทั้งหมด</ion-label>
+            <ion-toolbar>
+              <ion-segment value="all">
+                <ion-segment-button value="all" @click="allroom()">
+                  <ion-label>ห้องทั้งหมด</ion-label>
                 </ion-segment-button>
-                <ion-segment-button v-for="i in categorymenu" :key="i.name" :value="i.name" @click="filterRoom(i.category)">
-                <ion-label>{{ i.name }}</ion-label>
-              </ion-segment-button>
-          </ion-segment> 
+                <ion-segment-button v-for="i in roomtype" :key="i.room_type" :value="i.room_type"
+                  @click="filterRoom(i.room_type)">
+                  <ion-label>{{ i.room_type }}</ion-label>
+                </ion-segment-button>
+              </ion-segment>
             </ion-toolbar>
           </ion-row>
+        </ion-item>
+        <ion-grid>
+          <ion-item color="light">
+            <ion-col :sizeMd="8">
+            </ion-col>
+            <ion-col :sizeXs="8" :sizeMd="3">
+              <ion-searchbar></ion-searchbar>
+            </ion-col>
+            <ion-col :sizeXs="3" :sizeMd="1">
+              <ion-button color="purple" expand="block">ค้นหาห้อง</ion-button>
+            </ion-col>
           </ion-item>
-            <ion-grid>
-              <ion-item color="light">
-                <ion-col  :sizeMd="8">                  
-                </ion-col>
-                <ion-col :sizeXs="8" :sizeMd="3">
-                  <ion-searchbar></ion-searchbar>
-                </ion-col>
-                <ion-col :sizeXs="3" :sizeMd="1">                 
-                  <ion-button color="purple" expand="block">ค้นหาห้อง</ion-button>
-                </ion-col>
-              </ion-item>
-            </ion-grid>
-          <ion-card>                
-            <ion-grid>             
-              <ion-item>
-                <label>เลือกห้องพักที่ต้องการ</label>
+        </ion-grid>
+        <ion-card>
+          <ion-grid>
+            <ion-item>
+              <label>เลือกห้องพักที่ต้องการ</label>
               <ion-button slot="end" color="medium" size="small" routerLink="/testroomPage">จัดการห้อง</ion-button>
-              </ion-item>           
-              <ion-item color="light">
+            </ion-item>
+            <ion-item color="light">
               <ion-checkbox slot="start" color="light"></ion-checkbox>
-                <ion-col size="3">ห้อง</ion-col>
-                <ion-col size="3">สถานะ</ion-col>
-                <ion-col size="6">ประเภทห้อง</ion-col>  
-              </ion-item>
-              <ion-item  v-for="i in filteredroom" :key="i.no" @click="toroute(i.url)">
-              <ion-checkbox slot="start" value="allcheck"></ion-checkbox>
-                <ion-col size="3">{{ i.no }}</ion-col>              
-                <ion-col size="3" v-if="i.state === 1">
-                  <ion-badge color="medium">{{ i.status }}</ion-badge>
-                </ion-col> 
-                <ion-col size="3" v-if="i.state === 0">
-                  <ion-badge color="secondary">{{ i.status }}</ion-badge>
-                </ion-col>
-                <ion-col size="6">{{ i.type }}</ion-col>              
-              </ion-item>
-            </ion-grid>          
-          </ion-card>
+              <ion-col size="3">ห้อง</ion-col>
+              <ion-col size="3">สถานะ</ion-col>
+              <ion-col size="6">ประเภทห้อง</ion-col>
+            </ion-item>
+            <ion-item v-for="i in roomtype" :key="i.no" @click="toroute(i.url)">
+              <ion-col size="3">{{ i.room_id }}</ion-col>
+              <ion-col size="3" v-if="i.status === 0">
+                <ion-badge color="medium">ว่าง </ion-badge>
+              </ion-col>
+              <ion-col size="3" v-if="i.state === 1">
+                <ion-badge color="secondary">ไม่ว่าง</ion-badge>
+              </ion-col>
+              <ion-col size="3">{{ i.room_type }}</ion-col>
+              <ion-col>
+                <ion-button :routerLink="{
+                  name: 'id', params: {
+                    IDroom: i.room_id
+                  }
+                }">จัดการ</ion-button>
+              </ion-col>
+            </ion-item>
+          </ion-grid>
+        </ion-card>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
+import axios from 'axios';
 import { defineComponent } from 'vue';
 import { RouteLocationRaw, useRoute } from 'vue-router';
-import { IonBadge,IonButton,IonSearchbar,IonCard,IonItem,IonCheckbox,IonGrid,IonCol,IonSegment,IonSegmentButton,IonLabel,
-  IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonBadge, IonButton, IonSearchbar, IonCard, IonItem, IonCheckbox, IonGrid, IonCol, IonSegment, IonSegmentButton, IonLabel,
+  IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar
+} from '@ionic/vue';
 
 export default defineComponent({
   name: 'FolderPage',
-  components: { IonBadge,IonButton,IonSearchbar,IonCard,IonItem,IonCheckbox,IonGrid,IonCol,IonSegment,IonSegmentButton,IonLabel,
-    IonButtons,IonContent,IonHeader,IonMenuButton,IonPage,IonTitle,IonToolbar
+  components: {
+    IonBadge, IonButton, IonSearchbar, IonCard, IonItem, IonCheckbox, IonGrid, IonCol, IonSegment, IonSegmentButton, IonLabel,
+    IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar
   },
   data() {
-      return {
-        datatest: [
-          {no: '101',
-          status: 'ว่าง',
-          state :0,
-          price: '5,000',
-          type :'ห้องทั่วไป',
-          category: 1,
-          },
-          { no: '102',
-          status: 'ไม่ว่าง',
-          state :1,
-          price: '5,200',
-          type :'ห้องแอร์',
-          category: 2,
-          },
-          { no: '103',
-          status: 'ไม่ว่าง',
-          state :1,
-          price: '6,200',
-          type :'ห้อง VIP',
-          category: 3,
-          },
-          {no: '104',
-          status: 'ว่าง',
-          state :0,
-          price: '5,000',
-          type :'ห้องทั่วไป',
-          category: 1,
-          },
-          { no: '105',
-          status: 'ไม่ว่าง',
-          state :1,
-          price: '5,200',
-          type :'ห้องแอร์',
-          category: 2,
-          },
-          { no: '106',
-          status: 'ไม่ว่าง',
-          state :1,
-          price: '6,200',
-          type :'ห้อง VIP',
-          category: 3,
-          },
-          { no: '108',
-          status: 'ไม่ว่าง',
-          state :1,
-          price: '6,200',
-          type :'ห้อง VIP',
-          category: 3,
-          },
-        ],
-        categorymenu: [
-        {
-          name: 'ห้องทั่วไป',
-          category: 1,
-        },
-        {
-          name: 'ห้องแอร์',
-          category: 2,
-        },
-        {
-          name: 'ห้องVIP',
-          category: 3,
-        }
-      ],
-      filteredroom: {}
+    return {
+      roomtype: [],
+      filteredroom: {},
+
+
     }
   },
   methods: {
@@ -160,17 +109,32 @@ export default defineComponent({
       this.$router.push(rou)
     },
     allroom() {
-    this.filteredroom = this.datatest
+      this.filteredroom = this.roomtype
     },
-    filterRoom(iddata: number) {
-      console.log(iddata)
-      this.filteredroom = this.datatest.filter(item => item.category === iddata)
-    }
+    filterRoom(iddata: string) {
+      // console.log(iddata)
+      // console.log(this.filteredroom)
+      this.filteredroom = this.roomtype.filter((item: { room_type: string }) => item.room_type === iddata)
+      console.log(this.filteredroom)
+    },
+    async getDataFromDatabase() {
+      try {
+        const response = await axios.get(`https://demodate-549e4-default-rtdb.asia-southeast1.firebasedatabase.app/inst_room.json`);
+        this.roomtype = Object.values(response.data);
+        this.filteredroom = this.roomtype;
+        console.log(JSON.stringify(this.roomtype))
+      } catch (error) {
+        console.error(error);
+      }
+
+    },
+
+
   },
-  beforeMount(){
-    this.allroom()
+  created() {
+    this.getDataFromDatabase();
   },
-  
+
 });
 </script>
 
@@ -180,32 +144,34 @@ export default defineComponent({
   position: absolute;
   left: 0;
   right: 0;
- 
+
 
 }
+
 #container a {
   text-decoration: none;
 }
-ion-segment{
+
+ion-segment {
   size: 10px;
   text-align: left;
 }
 
-ion-searchbar.ios{
+ion-searchbar.ios {
   width: 80%;
 }
 
 .ion-color-purple {
---ion-color-base: var(--ion-color-purple);
---ion-color-base-rgb: var(--ion-color-purple-rgb);
---ion-color-contrast: var(--ion-color-purple-contrast);
---ion-color-contrast-rgb: var(--ion-color-purple-contrast-rgb);
---ion-color-shade: var(--ion-color-purple-shade);
---ion-color-tint: var(--ion-color-purple-tint);
+  --ion-color-base: var(--ion-color-purple);
+  --ion-color-base-rgb: var(--ion-color-purple-rgb);
+  --ion-color-contrast: var(--ion-color-purple-contrast);
+  --ion-color-contrast-rgb: var(--ion-color-purple-contrast-rgb);
+  --ion-color-shade: var(--ion-color-purple-shade);
+  --ion-color-tint: var(--ion-color-purple-tint);
 }
+
 ion-content {
   --background: #e7e6eb;
   --color: #fff;
 }
-
 </style>
